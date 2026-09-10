12edcd8f27cacfd926cd37ff178038d90eb6558e
minidu10 <minidudhananjna@gmail.com>
2026-09-10 17:31:59 +0530

    Initial production Linux server

---
+++ app/app.py
@@ -0,0 +1,30 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socket
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        hostname = socket.gethostname()
+
+        response = f"""
+        <html>
+            <head>
+                <title>DevOps Project</title>
+            </head>
+            <body>
+                <h1>🚀 My DevOps Server</h1>
+                <p>Application is running!</p>
+                <p>Server: {hostname}</p>
+            </body>
+        </html>
+        """
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(response.encode())
+
+server = HTTPServer(("0.0.0.0", 9999), Handler)
+
+print("Server running on port 8000...")
+
+server.serve_forever()
